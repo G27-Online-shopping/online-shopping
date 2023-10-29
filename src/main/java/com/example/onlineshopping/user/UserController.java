7682e77babf9be9064da29fc98df6af594d70f9e
Diyor <diyorbek2110@gmail.com>
2023-10-29 21:24:39 +0500

    To'gri busa kere

--- src/main/java/com/example/onlineshopping/user/UserController.java
+++ src/main/java/com/example/onlineshopping/user/UserController.java
@@ -1,8 +1,11 @@
 package com.example.onlineshopping.user;
 
+import com.example.onlineshopping.user.dto.UserCrateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -12,5 +15,15 @@ public class UserController {
     @GetMapping("sign-in")
     public String signIn(){
         return "user/sign-in";
+    }
+
+    @GetMapping("sign-up")
+    public String signUp(){
+        return "user/sign-up";
+    }
+    @PostMapping("sign-up")
+    public String signUp(@ModelAttribute UserCrateDto userCreatedDto){
+        userService.create(userCreatedDto);
+        return "redirect:user/sign-in";
     }
 }
