80707d0d0334c7ce206a4ceb2d6944bda7faf90c
Abror <yoldashevabror182@gmail.com>
2023-10-29 16:22:03 +0500

    Add sign-in add card

--- src/main/java/com/example/onlineshopping/card/CardController.java
+++ src/main/java/com/example/onlineshopping/card/CardController.java
@@ -2,9 +2,13 @@ package com.example.onlineshopping.card;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class CardController {
-
+    @GetMapping("card")
+    public String card(){
+        return "card/card";
+    }
 }
